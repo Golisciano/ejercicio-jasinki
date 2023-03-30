c34672eaef73e75415157eed5ec3edeaa0eeff59
Golisciano <goliscianomarcos@gmail.com>
2023-03-30 18:37:33 -0300

    primeros ejercicios

---
+++ src/as/Ejercicio1.java
@@ -0,0 +1,10 @@
+package as;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo"); 
+
+	}
+
+}
---
+++ src/as/Ejercicio2.java
@@ -0,0 +1,27 @@
+package as;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+	
+	
+
+		        String nombre = ""; 
+		        String apellido = ""; 
+
+		        Scanner scanner = new Scanner(System.in); 
+
+		        System.out.println("Ingrese Nombre");
+		        nombre = scanner.nextLine(); 
+
+		        System.out.println("Ingrese Apellido");
+		        apellido = scanner.nextLine(); 
+
+		        System.out.println ("Hola " + nombre + " "+ apellido); 
+
+		
+	}
+
+}
